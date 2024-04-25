3f5fd17ae4f51066b0dcc60f4c86bf241125ca41
tannyaarr <tanyachiras@gmail.com>
2024-04-25 10:43:30 +0200

    fixed syntax error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,11 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    // Create promises for signUpUser and uploadPhoto
     const signUpPromise = signUpUser(firstName, lastName);
     const uploadPromise = uploadPhoto(fileName);
 
-    // Execute promises and handle their results
     return Promise.allSettled([signUpPromise, uploadPromise])
         .then(results => {
-            // Map the results to the required structure
             return results.map(result => {
                 return {
                     status: result.status,
